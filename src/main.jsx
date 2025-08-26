5ff6049044bfd67f946ab10228a19fa0d91fd224
Naqeeb1124 <164079169+Naqeeb1124@users.noreply.github.com>
2025-08-26 07:41:04 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './i18n'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter basename="/Child-development-Project-Website">
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
